2cf372f509ac9a21cdab00983f3c1a3ca92a6c47
abdellahfihri <abdellahfihri@gmail.com>
2019-12-09 19:23:14 +0100

    Updates

---
+++ precache-manifest.f174913eaeb6b73776c1a1880c35c694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c74695e1515d03fb13cd3b72930f714",
+    "url": "/react.video.app/index.html"
+  },
+  {
+    "revision": "768f9ceec7b9cdfee44e",
+    "url": "/react.video.app/static/css/main.667ae0a1.chunk.css"
+  },
+  {
+    "revision": "d2274b02ea090db534d6",
+    "url": "/react.video.app/static/js/2.99a700a8.chunk.js"
+  },
+  {
+    "revision": "768f9ceec7b9cdfee44e",
+    "url": "/react.video.app/static/js/main.45657aef.chunk.js"
+  },
+  {
+    "revision": "4d5588c1d538036cf27a",
+    "url": "/react.video.app/static/js/runtime-main.43c41475.js"
+  }
+]);
